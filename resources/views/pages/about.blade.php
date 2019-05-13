fc6c1bf6b27882f1465fcf7a94372c848648853f
bakruhassan <bakru96@gmail.com>
2019-05-13 22:48:46 +0800

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-8 py-4">
+          <h4 class="text-white">About</h4>
+          <p class="text-muted">Legal Management System is a comprehensive legal case management application featuring all the essential elements of a quality Law Practice Management Solution. 
+              The Legal Management System revolves around the Case, Client, and Contacts databases. From these three screens you can control and work every Legal Case that is taken by the Law Firm / Law Department of any organisation.</p>
+        </div>
+        <div class="col-sm-4 py-4">
+          <h4 class="text-white">Contact</h4>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">Follow on Twitter</a></li>
+            <li><a href="#" class="text-white">Like on Facebook</a></li>
+            <li><a href="#" class="text-white">Email me</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <section class="text-center">
+              <h1 class="heading">Album example</h1>
+              <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sIet, but not too short so folks don't simply skip over it entirely.</p>
+              <p>
+                <a href="#" class="btn btn-primary">Main call to action</a>
+                <a href="#" class="btn btn-secondary">Secondary action</a>
+              </p>
+       </section>
+
+
+@endsection
